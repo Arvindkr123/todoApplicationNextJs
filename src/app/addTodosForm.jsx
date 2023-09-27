160578e411379ab728173cc12da0f05331539426
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-09-27 11:58:15 +0530

    solved problem getting while fetching data

--- src/app/addTodosForm.jsx
+++ src/app/addTodosForm.jsx
@@ -1,13 +1,52 @@
-import React from "react";
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 const addTodosForm = () => {
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+  const router = useRouter();
+  const onSumbmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("/api/newtask", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          description,
+        }),
+      });
+      const data = await res.json();
+      if (!data.success) return toast.error(data.message);
+      toast.success(data.message);
+      router.refresh();
+      setTitle("");
+      setDescription("");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
   return (
     <div className="login">
       <section>
-        <form>
+        <form onSubmit={onSumbmitHandler}>
           <h1>Add Todo</h1>
-          <input type="text" placeholder="enter task title" />
-          <input type="text" placeholder="enter task description" />
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            placeholder="enter task title"
+          />
+          <input
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            type="text"
+            placeholder="enter task description"
+          />
           <button type="submit">add Task</button>
         </form>
       </section>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,45 @@
 import { TodoItem } from "@/Components/ServerComponent";
 import Form from "./addTodosForm";
-export default function Home() {
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+const fetchTodo = async (token) => {
+  try {
+    const res = await fetch(`${process.env.URL}/api/mytask`, {
+      cache: "no-cache",
+      headers: {
+        cookie: `token=${token}`,
+      },
+    });
+    const data = await res.json();
+    if (!data.success) return [];
+    return data.tasks;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
+
+export default async function Home() {
+  const token = cookies().get("token")?.value;
+  if (!token) redirect("/login");
+  const tasks = await fetchTodo(token);
+  //console.log(tasks);
   return (
     <div className="container">
       <Form></Form>
       <section className="todosContainer">
-        <TodoItem
-          title={"do get job as soon as possible"}
-          description={"do get job as soon as possible"}
-          id={"sample id"}
-          completed={true}
-        />
+        {tasks.map((task) => {
+          return (
+            <TodoItem
+              title={task.title}
+              description={task.description}
+              id={task._id}
+              key={task._id}
+              completed={task.isCompleted}
+            />
+          );
+        })}
       </section>
     </div>
   );
